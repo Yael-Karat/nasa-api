57ebbeb424b4fcdc0cf0d6cc10e54f442708fb46
yaelk <yaelkara@edu.hac.ac.il>
2024-02-01 17:15:38 +0200

    adding api kay and fetch in js file

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const API_KEY = "8DWkvDy8fbVdx7mQKq8AqHKCCUc7bqnFHGY92v1s"
 
-/**
-function status(response) {
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector("#getdata").addEventListener("click", getData);
+});
+
+const status = (response) => {
     if (response.status >= 200 && response.status < 300) {
         return Promise.resolve(response)
     } else {
@@ -9,15 +12,32 @@ function status(response) {
     }
 }
 
-function json(response) {
-    return response.json()
+const toggleValidation = (element, show) => {
+    if (show) {
+        element.classList.add("is-invalid");
+        element.nextElementSibling.classList.remove("d-none");
+    } else {
+        element.classList.remove("is-invalid");
+        element.nextElementSibling.classList.add("d-none");
+    }
 }
+const getData = () => {
+    const userNameElement = document.getElementById("username");
+    const dataElement = document.getElementById("data");
+    // hide validation div
+    toggleValidation(userNameElement, false)
 
-fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=API_KEY')
-    .then(status)
-    .then(json)
-    .then(function (data) {
-        console.log('Request succeeded with NASA response', data);
-    }).catch(function (error) {
-    console.log('Request failed', error);
-});*/
+    dataElement.innerHTML = "<img src='img/loading-buffering.gif'>";
+    fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=API_KEY')
+        .then(status)
+        .then(res => res.json())
+        .then(json => {
+            dataElement.innerHTML = `User ${json.login} has ${json.followers} followers`; // remove the loading message
+        })
+        .catch(function (err) {
+            dataElement.innerHTML = 'Request failed';
+        }).finally(function () {
+        // always executed
+        userNameElement.value = "";
+    });
+}
